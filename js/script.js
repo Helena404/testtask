aa52ff607f4bf04be554b786fdbdd62624934d8a
Helena404 <hehelena404@gmail.com>
2024-05-27 01:45:40 +0300

    fix: fixed script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
     alert("Привет, мир");
 
     document.querySelector(".btn-warning").addEventListener("click", function() {
-		const block1 = document.querySelector(".row-1");
-		block1.classList.toggle('hidden');
+		document.querySelector(".row-1").classList.toggle('hidden');
     });
 
 
@@ -11,11 +10,8 @@
         const block2 = document.querySelector(".block-2");
         const block3 = document.querySelector(".block-3");
 
-		if (block2.classList.contains('order-1') && block3.classList.contains('order-2')) {
-			block2.classList.replace('order-1', 'order-2');
-			block3.classList.replace('order-2', 'order-1');
-		} else {
-			block2.classList.replace('order-2', 'order-1');
-			block3.classList.replace('order-1', 'order-2');
-		}
+		block2.classList.toggle('order-1');
+		block2.classList.toggle('order-2');
+		block3.classList.toggle('order-1');
+		block3.classList.toggle('order-2');
     });
